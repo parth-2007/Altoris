74ed92ee6b2fd4f611063df1d7c688eca90d512a
unknown <saxenaparth007@gmail.com>
2023-06-22 23:44:04 -0700

    code cleaning

--- retrieve.py
+++ retrieve.py
@@ -1,22 +1,22 @@
 import sqlite3
 import csv 
 
-def main():
-    conn = sqlite3.connect("chembl_33.db")
-    cursor = conn.cursor()
-    # Connect to chembl_33 db
+def query():
     n = input("Please enter the assay id:")
-    query = """
+    query = f"""
     SELECT cs.canonical_smiles, act.activity_id, act.assay_id, act.standard_relation,
         act.standard_value, act.standard_units, act.standard_type, act.molregno
     FROM Activities AS act
     JOIN compound_structures AS cs ON act.molregno = cs.molregno
     WHERE act.assay_id = {n}
     LIMIT 1;
-    """.format(n=n)
+    """
     # Made query and received assay id, execute query
     cursor.execute(query)
     results = cursor.fetchall()
+
+    return results
+def genCSV():
     output_file = "output.csv"
     # Generate and fill up CSV file with recently pulled data
     with open(output_file, 'w', newline='') as csvfile:
@@ -24,7 +24,7 @@ def main():
         writer.writerow(['Canonical Smiles', 'Activity ID', 'Assay ID', 'Standard Relation',
                         'Standard Value', 'Standard Units', 'Standard Type', 'Molregno'])
         writer.writerows(results)
-
+def printResults():
     # Print the results in command prompt
     for row in results:
         canonical_smiles, activity_id, assay_id, standard_relation, \
@@ -38,11 +38,13 @@ def main():
         print("Standard Units:", standard_units)
         print("Standard Type:", standard_type)
         print("Molregno:", molregno)
-        
-    # End script
-    cursor.close()
-    conn.close()
 
 
 if __name__ == '__main__':
-    main()
+    conn = sqlite3.connect("chembl_33.db")
+    cursor = conn.cursor()
+    results = query()
+    genCSV()
+    printResults()
+    cursor.close()
+    conn.close()
